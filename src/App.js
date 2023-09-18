25f3b6100181ce608d362dd0f6f8e03ef383ffb5
eshapatel2005 <esha.patel5605@gmail.com>
2023-09-18 13:36:29 +0530

    React JS Project-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Navs from './components/fronted';
+
 
 function App() {
+
+  let obj={
+   
+    img1:'https://i.pinimg.com/originals/9c/d1/bf/9cd1bf6c2d1a88e8ac473f62a2898c62.png',
+    menu:'MENU',
+    location:'LOCATION',
+    about:'ABOUT',
+    contact:'CONTACT',
+    text1:'YOUR FEET DESERVE THE BEST',
+    img2:'https://logos-world.net/wp-content/uploads/2020/11/Flipkart-Emblem.png',
+    img3:'https://i.pinimg.com/originals/01/ca/da/01cada77a0a7d326d85b7969fe26a728.jpg',
+    img4:'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/1752710/2017/3/9/11489041755548-Nike-Men-Red-Solid-Air-Force-1-07-Suede-Sneakers-5411489041755150-1.jpg',
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Navs {...obj}></Navs>
   );
 }
 
---
+++ src/components/fronted.js
@@ -0,0 +1,56 @@
+import './fronted.css';
+
+export default function Navs(props){
+
+    return(
+        <>
+        
+           <div>
+            <nav className="container">
+                <div className="logo">
+                   
+                    <img src={props.img1} className="img2"></img>
+                </div>
+
+                <ul>
+                    <li href="#">{props.menu}</li>
+                    <li href="#">{props.location}</li>
+                    <li href="#">{props.about}</li>
+                    <li href="#">{props.contact}</li>
+                </ul>
+
+                <button>LOGIN</button>
+            </nav>
+           </div>
+
+           <main className="hero container">
+            <div className="hero-content">
+                <h1>{props.text1}</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.</p>
+
+                <div className="hero-btn">
+                    <button>Shop Now</button>
+                    <button className="secondary-btn">Category</button>
+                </div>
+
+                <div className="shopping">
+                    <p>Also Available On </p>
+                    <div className="brand-icons">
+                        <img src={props.img2} className="img1"></img>
+                        <img src={props.img3} className="img1"></img>
+                    </div>
+                </div>
+            </div>
+
+           
+
+
+            <div className="hero-image">
+                <img src={props.img4} className="img3"></img>
+            </div>
+           </main>
+
+        </>
+    );
+
+}
